e6141f9d2cbbc1a3c34544b05629a79be8ecbc48
Dicky Nainggolan <dicky.nbisnis@gmail.com>
2023-10-17 10:05:17 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1>hello world</h1>
+    <h2>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique reiciendis quos mollitia repellendus recusandae, voluptatem, harum libero adipisci minus hic ipsum, ut in ratione nobis magnam enim? Qui, neque suscipit.</h2>
 </body>
 </html>
